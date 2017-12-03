35408709589f7938a7b9f32541845f067153c957
Shirley91 <yarulin91@gmail.com>
2017-12-02 20:31:14 -0600

    SVM timing

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -26,9 +26,18 @@ features_train, features_test, labels_train, labels_test = preprocess()
 ### your code goes here ###
 from sklearn import svm
 clf = svm.SVC(kernel="linear")
+
+t0 = time()
 clf.fit(features_train,labels_train)
+print "training time: ", round(time()-t0,3), "s"
+
+t1 = time()
 clf.predict(features_test)
-print "accuracy: ", clf.score(features_test,labels_test)
+print "predicting time: ", round(time()-t1,3), "s"
+
+t2 = time()
+print "accuracy: ", clf.score(features_test, labels_test)
+print "scoring time: ", round(time()-t2,3), "s"
 #########################################################
 
 
