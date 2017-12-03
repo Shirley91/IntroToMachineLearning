504d780e65fe4f3bb905a0824760f874d2cf928b
Shirley91 <yarulin91@gmail.com>
2017-12-02 19:39:38 -0600

    calculate the accuracy

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -26,8 +26,10 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
-
-
+from sklearn.naive_bayes import GaussianNB
+clf = GaussianNB()
+clf.fit(features_train,labels_train)
+print "accuracy: ", clf.score(features_test, labels_test)
 #########################################################
 
 
