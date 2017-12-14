5f22ca45dd64a45fdef6970c03ef441010113653
Shirley91 <yarulin91@gmail.com>
2017-12-13 22:16:05 -0600

    query the dataset

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -20,9 +20,27 @@ import pickle
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
 people = 0
+poiCount = 0
+
+print "features for each person", len(enron_data["SKILLING JEFFREY K"])
 
 for person in enron_data:
 	people += 1
+	if enron_data[person]['poi'] == 1:
+		poiCount += 1
 
 print "people:", people 
+print "Enron data poi count:", poiCount
 
+poiFile = open("../final_project/poi_names.txt")
+poiCount_total = 0
+lines = poiFile.readlines()
+for line in lines:
+	if line[0] == '(':
+		poiCount_total +=1
+		
+print "Total POIs:", poiCount_total
+
+print "James Prentice:", enron_data["PRENTICE JAMES"]["total_stock_value"]
+print "Wesley Colwell:", enron_data["COLWELL WESLEY"]["from_this_person_to_poi"]
+print "Jeffrey K Skilling:", enron_data["SKILLING JEFFREY K"]["exercised_stock_options"]
