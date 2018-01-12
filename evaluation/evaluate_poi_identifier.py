aaef6be11510128ec9b167d7daeda0e174431990
Shirley91 <yarulin91@gmail.com>
2018-01-11 19:47:53 -0600

    evaluate by accuracy, precision, and recall

--- evaluation/evaluate_poi_identifier.py
+++ evaluation/evaluate_poi_identifier.py
@@ -28,4 +28,47 @@ labels, features = targetFeatureSplit(data)
 
 ### your code goes here 
 
+from sklearn.cross_validation import train_test_split
+features_train, features_test,labels_train, labels_test = train_test_split (features,\
+	labels, test_size = 0.3, random_state = 42)
 
+from sklearn.tree import DecisionTreeClassifier
+
+clf = DecisionTreeClassifier()
+clf.fit(features_train,labels_train)
+print "accuracy: ", clf.score(features_test, labels_test)
+
+pred = clf.predict(features_test)
+
+num_pois = 0
+
+for i in pred:
+	if i == 1:
+		num_pois += 1
+
+print "number of POIs predicted: ", num_pois
+print "total number of people in test set:", len(pred)
+
+true_positives = 0
+false_positives = 0
+false_negatives = 0
+
+for i, j in zip(pred,labels_test):
+	if i == 1 and j == 1:
+		true_positives += 1
+	elif i == 1 and j == 0:
+		false_positives += 1
+	elif i == 0 and j == 0:
+		false_negatives += 1
+
+print "true positives: ", true_positives
+print "false positives: ", false_positives
+print "false negatives: ", false_negatives
+
+print "precision: ", true_positives /(true_positives + false_positives)
+print "recall: ", true_positives / (true_positives + false_negatives)
+
+# another way 
+from sklearn import metrics
+print "precision: ", metrics.precision_score(labels_test, pred)
+print "recall: ", metrics.recall_score(labels_test, pred)
